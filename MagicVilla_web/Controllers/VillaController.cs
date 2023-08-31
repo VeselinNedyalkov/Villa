246a306976ce937ef31094c7387a73ff91a5ff5a
VesoVn <vnedyalkov.85@yahoo.com>
2023-08-31 14:12:13 +0300

    Add Create and Update Villa

---
+++ MagicVilla_web/Controllers/VillaController.cs
@@ -0,0 +1,92 @@
+﻿using AutoMapper;
+using MagicVilla_web.Models.DTO;
+using MagicVilla_web.Services.Contacts;
+using MagicVilMagicVilla_web.Models;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Reflection.Metadata.Ecma335;
+
+namespace MagicVilla_web.Controllers
+{
+    public class VillaController : Controller
+    {
+        private readonly IVillaSurvice villaSurvice;
+        private readonly IMapper mapper;
+
+        public VillaController(IVillaSurvice villaSurvice, IMapper mapper)
+        {
+            this.villaSurvice = villaSurvice;
+            this.mapper = mapper;
+        }
+
+        public async Task<IActionResult> IndexVilla()
+        {
+            List<VillaDTO> list = new List<VillaDTO>();
+
+            var response = await villaSurvice.GetAllAsync<APIResponse>();
+
+            if (response != null && response.IsUsccess)
+            {
+                list = JsonConvert.DeserializeObject<List<VillaDTO>>(Convert.ToString(response.Result));
+            }
+
+            return View(list);
+        }
+
+        
+        public async Task<IActionResult> CreateVilla()
+        {
+
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateVilla(VillaCreateDTO model)
+        {
+            if(ModelState.IsValid)
+            {
+                var response = await villaSurvice.CreateAsync<APIResponse>(model);
+
+                if (response != null && response.IsUsccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+
+            return View(model);
+        }
+
+        public async Task<IActionResult> UpdateVilla(int villaId)
+        {
+            var response = await villaSurvice.GetAsync<APIResponse>(villaId);
+
+            if (response != null && response.IsUsccess)
+            {
+                VillaDTO model = JsonConvert.DeserializeObject<VillaDTO>(Convert.ToString(response.Result));
+                return View(mapper.Map<VillaUpdateDTO>(model));
+            }
+
+            return NotFound();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> UpdateVilla(VillaUpdateDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await villaSurvice.UpdateAsync<APIResponse>(model);
+
+                if (response != null && response.IsUsccess)
+                {
+                    return RedirectToAction(nameof(IndexVilla));
+                }
+            }
+
+            return View(model);
+        }
+    }
+}
---
+++ TrainingAPI/Migrations/20230831105331_changeNullableToFalse.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TrainingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class changeNullableToFalse : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Amenity",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SpecialDeails",
+                table: "VillaNumbers",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedTime", "UpdatedTime" },
+                values: new object[] { new DateTime(2023, 8, 31, 13, 53, 31, 454, DateTimeKind.Local).AddTicks(1193), new DateTime(2023, 8, 31, 13, 53, 31, 454, DateTimeKind.Local).AddTicks(1234) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedTime", "UpdatedTime" },
+                values: new object[] { new DateTime(2023, 8, 31, 13, 53, 31, 454, DateTimeKind.Local).AddTicks(1238), new DateTime(2023, 8, 31, 13, 53, 31, 454, DateTimeKind.Local).AddTicks(1239) });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Details",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Amenity",
+                table: "Villas",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<string>(
+                name: "SpecialDeails",
+                table: "VillaNumbers",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "CreatedTime", "UpdatedTime" },
+                values: new object[] { new DateTime(2023, 8, 29, 15, 16, 18, 371, DateTimeKind.Local).AddTicks(2357), new DateTime(2023, 8, 29, 15, 16, 18, 371, DateTimeKind.Local).AddTicks(2401) });
+
+            migrationBuilder.UpdateData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "CreatedTime", "UpdatedTime" },
+                values: new object[] { new DateTime(2023, 8, 29, 15, 16, 18, 371, DateTimeKind.Local).AddTicks(2404), new DateTime(2023, 8, 29, 15, 16, 18, 371, DateTimeKind.Local).AddTicks(2406) });
+        }
+    }
+}
